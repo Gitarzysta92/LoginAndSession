d6aa5ae5f25ee9ec139b0ae314b7494c5cac5266
Michał <michal@threesixty.com>
2018-11-09 18:27:36 +0100

    Separate dirWalker module

--- core/app.js
+++ core/app.js
@@ -4,6 +4,78 @@ const methods = require('../helpers/methods.js');
 const fs = require('fs');
 const path = require('path');
 
+
+class DirMap {
+	constructor(startDir) {
+		this.directory = startDir;
+		this.dirModel = [];
+		this.walkerPromises = [];
+	
+		this.walker(this.directory);
+		//Promise.all(this.walkerPromises).then(x => console.log(x));
+	}
+/*
+	async recursiveDirWalker() {
+		const self = this;
+		await function recursiveWalker(directory) {
+			const injectDir = directory;
+			const initPromise = new Promise((resolve, reject) => {
+				walker(resolve)	
+			});
+			return readPromise;
+		}
+	}
+*/
+	walker(directory) {
+		fs.readdir(directory, (err, result) => {
+			if (err) {
+				throw err;
+			} else {
+				result.forEach((item, arr, key) => {
+					const currentPath = path.resolve(directory, item); 
+					const checkDir = new Promise((resolve, reject) => {
+						fs.stat(currentPath, (err, stats) => {
+							if (err) {
+								reject(err)
+							} else {
+								if (stats.isDirectory() && this.dirValidation(currentPath)) {
+									resolve(currentPath);
+									this.dirModel.push(currentPath);
+									//this.recursiveDirWalker(currentPath);
+								} else if(stats.isFile()) {
+									this.dirModel.push(currentPath);
+									resolve(currentPath);
+									//this.recursiveDirWalker();
+								} 
+							}
+						});
+					}).then(x => console.log(this));
+					
+					//if (Object.is(arr.length -1, key)) {
+					//	resolve(this.dirModel);
+					//}
+				});
+			}
+		});	
+	}
+		
+
+	dirValidation(directory) {
+		const node = /(node_modules|\.git)$/i;
+		return !node.test(directory);
+	}
+
+	fileValidation() {
+		const file = /as$/;
+		return !file.test(filePath);
+	}
+
+}
+
+
+
+
+
 class App {
 	constructor(args) {
 		this.root_dir = args[0];
@@ -11,7 +83,10 @@ class App {
 		this.method = db_methods.bind(this);
 		this.methods = methods;
 
-		this.modules = ['helpers'];
+		this.modules = new DirMap(this.root_dir);
+
+		//console.log(this.modules.dirModel);
+
 	}
 
 	met() {
@@ -20,12 +95,38 @@ class App {
 		return this.method;
 	}
 
-	recursiveDirectoriesWalker(directory, callback) {
-		fs.readdir(directory, function(err, result){
-			result.forEach(item => )
-		});			
+}
 
+
+
+
+
+
+module.exports = (function() {
+	
+	this.app;
+
+
+	function createInstance(args) {
+		const app = new App(args);
+		return app;
 	}
+
+
+	return {
+		init: function(...args) {
+				this.app = createInstance(args);
+				this.app.modules
+
+				delete this.init;
+			}
+	}
+
+})();
+
+
+
+/*
 
 	registerModules() {
 		const items = [];
@@ -71,36 +172,10 @@ class App {
 				.then(item => {
 					//const modulePath = path.resolve()
 					found.push(item); 
-					console.log(item);
+					//console.log(item);
 				});
 		})
 		return found;
 	}
 
-}
-
-
-
-
-
-
-module.exports = (function() {
-	
-	this.app;
-
-
-	function createInstance(args) {
-		const app = new App(args);
-		return app;
-	}
-
-
-	return {
-		init: function(...args) {
-				this.app = createInstance(args);
-				this.app.registerModules();
-				delete this.init;
-			}
-	}
-
-})();
+*/
